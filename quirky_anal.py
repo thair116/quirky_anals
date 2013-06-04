768b06ef1ed74f71f433df505e5b17a1d8f36166
Travis Hairfield <twh116@gmail.com>
2013-06-04 08:52:57 -0400

    Add title scraping

--- quirky_anal.py
+++ quirky_anal.py
@@ -1,22 +1,30 @@
 import time
 import requests
+import re
 from bs4 import BeautifulSoup
 
-r = requests.get("http://www.quirky.com/products/1/timeline")
-soup = BeautifulSoup(r.text)
+for x in range(1,6):
+	URL = "http://www.quirky.com/products/%d/timeline" % (x)
+	r = requests.get(URL)
+	soup = BeautifulSoup(r.text)
 
-timeline = soup.find(class_="horizontal ui timeline eight events")
+	print("\n ****** Quirky Analysis - %d ***** \n") % (x)
 
-print("\n ****** Quirky Analysis ***** \n")
+	
+	title = soup.head.title
+	titleText = title.text.replace(' Timeline | Quirky','')
+	print(titleText)
 
-for event in timeline.find_all(class_="event"):
-	#print(event.div)
-	print(event.find(class_="name").text)
-	#print(event['data-title'])
-	date = event['data-status'].replace('Completed: ','').replace(',','')
-	print(date)
-	date2 = time.strptime(date, "%b %d %Y")
-	print(date2)
-	print("\n------------------------------------------------\n")
+	timeline = soup.find(class_="timeline")
+
+	for event in timeline.find_all(class_="event"):
+		#print(event.div)
+		print(event.find(class_="name").text)
+		#print(event['data-title'])
+		date = event['data-status'].replace('Completed: ','').replace(',','')
+		print(date)
+		date2 = time.strptime(date, "%b %d %Y")
+		#print(date2)
+		#print("\n------------------------------------------------\n")
 
 
