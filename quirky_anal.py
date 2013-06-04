52e79a2f20021ea0d054f4e9ce44f70477b5f5bb
Travis Hairfield <twh116@gmail.com>
2013-06-04 12:17:00 -0400

    Include output to MySQL database

--- quirky_anal.py
+++ quirky_anal.py
@@ -1,33 +1,50 @@
 import time
 import requests
-import re
+import MySQLdb as mdb
+import sys
 from bs4 import BeautifulSoup
 
 redirectTitle = "Inventions | Submit, Vote & Collaborate at Quirky"
 homeTitle = "Quirky Makes Invention Accessible"
 emptyStr = ""
 
-for x in range(653,661):
+
+con = mdb.connect('localhost', 'testuser', 'test623', 'testdb');
+
+
+cur = con.cursor()
+#cur.execute("DROP TABLE IF EXISTS Quirky_data")
+#cur.execute("CREATE TABLE Quirky_data(Id INT PRIMARY KEY AUTO_INCREMENT, \
+ #            Name VARCHAR(25), Stage VARCHAR(25), _DATE DATE)")
+
+for x in range(194,700):
 	URL = "http://www.quirky.com/products/%d/timeline" % (x)
 	r = requests.get(URL)
 	soup = BeautifulSoup(r.text)
 
-	print("\n ****** Quirky Analysis - %d ***** \n") % (x)
+	
 
 	title = soup.head.title
 	titleText = title.text.replace(' Timeline | Quirky','')
 	
 	if (titleText != redirectTitle) and (titleText != homeTitle):
-		print(titleText)
+		
+		
+		#print(titleText)
 		timeline = soup.find(class_="timeline")
 		for event in timeline.find_all(class_="event"):
-			print(event.find(class_="name").text)
+			stage = event.find(class_="name").text
+			#print(stage)
 			#print(event['data-title'])
-			date = event['data-status'].replace('Completed: ','').replace(',','')
-			if date != emptyStr:
-				print(date)
-				date2 = time.strptime(date, "%b %d %Y")
+			dateStr = event['data-status'].replace('Completed: ','').replace(',','').replace('In Judgment: ','')
+			if dateStr != emptyStr:
+				#print(dateStr)
+				date_object = time.strptime(dateStr, "%b %d %Y")
+				#print(date_object.tm_year)
+				date_sql = "%d-%d-%d" % (date_object.tm_year, date_object.tm_mon, date_object.tm_mday)
+				cur.execute("INSERT INTO Quirky_data(Name, Stage, _Date) VALUES(%s, %s, %s)",(titleText, stage, date_sql))
 			else:
+				cur.execute("INSERT INTO Quirky_data(Name, Stage) VALUES(%s, %s)",(titleText, stage))
 				print("ERROR - no date found")
 			#print(date2)
 			#print("\n------------------------------------------------\n")
@@ -35,4 +52,8 @@ for x in range(653,661):
 		print("\n ----- End of Products - %d Found----- \n") % (x)
 		break
 
+	con.commit()
+	print("\n ****** Quirky Analysis - %d Done ***** \n") % (x)
+
+con.close()
 
